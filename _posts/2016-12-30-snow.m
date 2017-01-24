58a7a4aea940e07d5af303fb015fe242529405b7
Gabriel Florit <gabrielflorit@gmail.com>
2017-01-24 14:54:46 -0500

    Create 2016-12-30-snow.m

---
+++ _posts/2016-12-30-snow.m
@@ -0,0 +1,6 @@
+---
+layout: default
+link: https://apps.bostonglobe.com/metro/graphics/2016/12/snow-totals
+image: https://cloud.githubusercontent.com/assets/370976/22264057/b18deed6-e244-11e6-8db2-843cc067c7e2.gif
+github: https://github.com/BostonGlobe/snow
+---
